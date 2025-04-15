6a7e772ee513d782f5f3f20c9f8fb44c226ea617
JakeHenley <jhenle20@students.kennesaw.edu>
2025-04-14 20:49:53 -0400

    Update main.py
    adjusting aces from 11 to 1 to avoid going over 21 and busting

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ import random
 def deal_card():
     cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
     return random.choice(cards)
+def calculate_score(cards):
+    score = sum(cards)
+    aces = cards.count(11)
+
+    while score > 21 and aces:
+        score -= 10  # Change one Ace from 11 to 1
+        aces -= 1
