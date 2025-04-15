b06c55b28862bdffe2fc3edbbd0730c0997c3b32
JakeHenley <jhenle20@students.kennesaw.edu>
2025-04-14 20:56:58 -0400

    Update main.py
    These lines of code determines if player is a winner or loser. It also contains the messages the player will receive dependent on if they win or lose.

--- main.py
+++ main.py
@@ -17,3 +17,15 @@ def calculate_score(cards):
 def compare(player_score, computer_score):
     if player_score == computer_score:
         return "Draw"
+elif computer_score == 0:
+        return "Lose, opponent has Blackjack!"
+    elif player_score == 0:
+        return "Win with a Blackjack!"
+    elif player_score > 21:
+        return "You went over. You lose!"
+    elif computer_score > 21:
+        return "Opponent went over. You win!"
+    elif player_score > computer_score:
+        return "You win!"
+    else:
+        return "You lose!"
